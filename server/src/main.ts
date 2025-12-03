967a43a2f866339b845701f3cc5a1aabdb4f5cfe
batanai <user@Mac.home>
2025-12-03 16:31:55 +0200

    port logging

--- server/src/main.ts
+++ server/src/main.ts
@@ -30,6 +30,8 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true
   }));
-  await app.listen(process.env.PORT ?? 3300, '0.0.0.0');
+  const port = process.env.PORT ?? 3300;
+  await app.listen(port, '0.0.0.0');
+  console.log(`Server running on port ${port}`);
 }
 bootstrap();
